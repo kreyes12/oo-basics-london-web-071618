ac6cb9679b5de34272dae3966b950c253ba519d5
Karla R <karla.y.reyes12@gmail.com>
2018-07-18 11:42:40 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -16,6 +16,25 @@ class Book
     @author
   end
   
+    def page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
+  end
+ 
+  def genre=(genre)
+    @genre = genre
+  end
+ 
+  def genre
+    @genre
+  end
+ 
+  def turn_page
+  end
+  
 end
 
 
